403459ba46c108993d7609166676ece6befc6ea0
reiparas <riva.paras@student.ateneo.edu>
2026-03-12 17:11:00 +0800

    Adjusted the location of URLS from one urls.py to another and fixed the password reset email template.

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -23,4 +23,24 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ledger.urls')),
+
+    path('login/',
+        auth_views.LoginView.as_view(),
+        name='login'),
+    path('logout/',
+        auth_views.LogoutView.as_view(next_page='login'),
+        name='logout'),
+
+    path("password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset"),
+    path("password_reset_done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done"),
+    path("reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm"),
+    path("reset_done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete"),
 ]
